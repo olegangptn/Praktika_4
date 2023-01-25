d0fca0d3f09eec749e2b18f87068d82e7869099b
OLGPTN <oleg_shulaev@mail.ru>
2023-01-25 06:41:16 +0300

    first commit

---
+++ Exercise_25/Program 25.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+    
+
+Console.WriteLine ("Введите число");
+  
+int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine ("Введите степень");
+  
+int stepen = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+    for (int i=0; i < stepen; i++)
+    {
+      result = result*num;
+    }
+  Console.WriteLine(result);
---
+++ Exercise_27/Program_27.cs
@@ -0,0 +1,10 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. 
+
+Console.Write("Введите число: "); 
+string value = Console.ReadLine();
+ int sum = 0; 
+ for (int i = 0; i < value.Length; i++) 
+ { 
+    int number = int.Parse(Convert.ToString(value[i])); sum += number; 
+    } 
+ Console.Write(sum);
